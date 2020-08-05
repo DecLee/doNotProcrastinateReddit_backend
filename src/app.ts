3d67d392c110cd51216027d76613654e4021c6ff
declee <derricklee6@gmail.com>
2020-08-05 15:12:02 -0700

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import passport from 'passport';
+import crypto from 'crypto';
+
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
+});
